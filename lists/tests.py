c0b11574a54c6549fc9d9217d1945cd4db9b0ffb
Marat Ayaibergenov <marataya@gmail.com>
2024-05-25 20:35:31 +0500

    Add lists app with unit tests, change / url to home_page

---
+++ lists/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.http import HttpRequest, HttpResponse
+
+from lists.views import home_page
+# Create your tests here.
+class HomePageTest(TestCase):
+    def test_home_page_is_about_todo(self):
+        request = HttpRequest()
+        response: HttpResponse = home_page(request)
+
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
---
+++ lists/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_page(request: HttpRequest):
+    return HttpResponse('<html><title>To-Do Lists</title></html>')

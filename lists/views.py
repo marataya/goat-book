a5aa93acb7224bc78c19676781acf913515617f9
Marat Ayaibergenov <marataya@gmail.com>
2024-05-28 01:08:37 +0500

    more html in homepage template, add csrf_exempt to view

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def home_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'home.html')
